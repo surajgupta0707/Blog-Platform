832542e2fc0dd9e6928b0dccc5e9348f9d9dafd0
Suraj <surajgupta68420@gmail.com>
2026-05-29 14:33:23 +0530

    Fix crypto error by downgrading Express to stable 4.18.2 and adding crypto import

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express   = require('express');
 const cors      = require('cors');
 const dotenv    = require('dotenv');
 const path      = require('path');
+const crypto    = require('crypto');
 const connectDB = require('./config/db');
 
 dotenv.config();
